91f0e176d34e25b21286635c7a886eb07055a5a7
Fernando Cunha <fernando.cunha@traderev.com>
2020-03-17 22:10:53 -0400

    Added registration page and route

--- application.py
+++ application.py
@@ -6,4 +6,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    """Creates the user"""
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+    #Gets form information
+    if request.form.get("name"):
+        name = request.form.get("name")
+        print(name)
+    else:
+        name = None
+    if request.form.get("password"):
+        password = request.form.get("password")
+        print(password)
+    
+    return render_template("register.html", name=name)
