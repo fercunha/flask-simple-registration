deaf219be9f749b2911e19344736158a0be5c474
Fernando Cunha <fernando.cunha@traderev.com>
2020-03-18 21:28:47 -0400

    Working registration page with no verification

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 import mydb
+import datetime
 
 mydb.create()
 
@@ -22,27 +23,28 @@ def register():
     """Creates the user"""
 
     if request.method == "GET":
-        if request.form.get("user"):
-            name = request.form.get("user")
-            return render_template("register.html", name=name)
+        if request.form.get("username"):
+            username = request.form.get("username")
+            return render_template("register.html", name=username)
         else:
             return render_template("register.html")
     else:
         #Gets form information
-        if request.form.get("firstName"):
-            firstName = request.form.get("firstName")
-            print(firstName)
-        if request.form.get("lastName"):
-            lastName = request.form.get("lastName")
-            print(lastName)
-        if request.form.get("user"):
-            name = request.form.get("user")
-            print(name)
-        if request.form.get("password"):
-            password = request.form.get("password")
-            print(password)
-
-        return render_template("success.html")
+        name = request.form.get("name")
+        lastname = request.form.get("lastname")
+        username = request.form.get("username")
+        # checkusername = mydb.db.execute("SELECT * FROM users").fetchone()
+        password = request.form.get("password")
+        try:
+            registration_date = datetime.datetime.now()
+            print(registration_date)
+            mydb.db.execute("INSERT INTO users (name, lastname, username, password, registration_date) VALUES (:name, :lastname, :username, :password, :registration_date)",
+                {"name": name, "lastname": lastname, "username": username, "password": password, "registration_date": str(registration_date)})
+            mydb.db.commit()
+            message = "User " + username + " registered successfully!"
+            return render_template("success.html", message=message)
+        except:
+            return render_template("error.html", message='Registration failed.'), 400
 
 if __name__ == "__main__":
     main()
--- mydb.py
+++ mydb.py
@@ -2,10 +2,14 @@ import sqlalchemy
 import sqlalchemy.dialects
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
 from sqlalchemy.orm import scoped_session, sessionmaker
+import logging
+
+# logging.basicConfig()
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
 
 # engine = create_engine(os.getenv("DATABASE_URL"))
 DATABASE_URL = 'postgres://oqnyzoveayjpnb:35377aea43fe446da6620fc909e6cb5176fc2ff0d48e9eed0cc1e248caf979a0@ec2-52-203-160-194.compute-1.amazonaws.com:5432/d1hkt2ecuhb45o'
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 db = scoped_session(sessionmaker(bind=engine))
 
 def create():
@@ -15,9 +19,9 @@ def create():
        Column('id', Integer, primary_key = True), 
        Column('name', String), 
        Column('lastname', String),
-       Column('user', String),
+       Column('username', String),
        Column('password', String),
-       Column('registration_date', DateTime(timezone=False))
+       Column('registration_date', DateTime(timezone=True))
     )
     meta.create_all(engine)
 
