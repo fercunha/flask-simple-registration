b5433c2f98bd7810a83897fa3bf4fde95ad42341
Fernando Cunha <fernando.cunha@traderev.com>
2020-03-19 21:43:40 -0400

    Added password hashing in the backend

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import mydb
 import datetime
+from passlib.hash import sha256_crypt
 
 mydb.create()
 
@@ -20,11 +21,22 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    if mydb.db.execute("SELECT * FROM users WHERE username = :username AND password = :password",
-        {"username": username, "password": password}).rowcount == 1:
-        return render_template("success.html", message='Login successful!')
+    user_infos = mydb.db.execute("SELECT username, password FROM users WHERE username = :username", {"username": username}).fetchall()
+    if len(user_infos) == 1:
+        for info in user_infos:
+            saved_password = info.password
+            if sha256_crypt.verify(password, saved_password):
+                return render_template("success.html", message='Login successful!')
+            else:
+                return render_template("login.html", error_message='Wrong username or password'), 403
     else:
-        return render_template("login.html", error_message='Wrong username or password'), 403
+        return render_template("login.html", error_message='Duplicate user!'), 409
+
+    # if mydb.db.execute("SELECT * FROM users WHERE username = :username AND password = :password",
+    #     {"username": username, "password": password}).rowcount == 1:
+    #     return render_template("success.html", message='Login successful!')
+    # else:
+    #     return render_template("login.html", error_message='Wrong username or password'), 403
     
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -45,6 +57,7 @@ def register():
         lastname = request.form.get("lastname")
         username = request.form.get("username")
         password = request.form.get("password")
+        password = sha256_crypt.hash(password)
 
         #Check if username already exists
         print(f"Checking if user {username} exists.")
